43ce343361f7a1bbd12a9a4b66c158b06af3fa1d
naomigonzalez <gon21043@byui.edu>
2022-06-20 13:20:06 -0500

    reacomodo

--- flask/start.py
+++ flask/start.py
@@ -11,7 +11,7 @@ def create_app():
 
 app = create_app()
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET']) # get obtener la información de un end point 
 def get_inmuebles():
 
     """End-point para realizar la búsqueda de inmuebles"""
@@ -20,15 +20,32 @@ def get_inmuebles():
     print("ENTROOOOOOOOOOOOOOOOOOO")
     return "<h1><b>Hola k aze?</b></h1>"
 
-@app.route('/crea_usuario', methods=['POST'])
-def crea_usuario():
+@app.route('/crea_usuario', methods=['POST']) # crear nuevos atributos en una base de datos 
+def crea_usuario_metodo():
 
-    json = request.get_json()
-    print(f"crea_usuario ==============> {json}")
+    json:dict = request.get_json()
+    # print(f"crea_usuario ==============> {json}")
+
+    # print(json.get('nombre')) # si esta variable no existiera saldría none. 
+    # # print(json['nombre2']) la forma NO recomendada porque sino existe marca un error 
+    # print(json.get('edad'))
+    # # print(json.get('edad1'))
+    # print(json.get('archivo'))
+
+    f = open(f"{json.get('archivo')}.txt", "x")
+    f.write(f"{json.get('nombre')}")
+    f.write(f"{json.get('edad')}")
+
+    f.close
+
+
     return {
       "codigo": 200,
-      "respuesta": f"usuario creado :: {json.get('mensaje')}"
+      "respuesta": f"usuario {json.get('nombre')} ha sido creado en el archivo {json.get('archivo')}"
     }
+
+
+
 
 
 
